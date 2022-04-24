86c3493b269de6723d2d94aa2953fb71de5f1515
mihayounas <mihaelayounas55@gmail.com>
2022-04-24 15:21:21 +0000

    Add syntax semicolons to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 //** Wait for Dom to load before running the game*/
 document.addEventListener("DOMContentLoaded", function() {
-    //Create pop up modal function
+    //** Create pop up modal function*/
     var btn = document.getElementById("myBtn");
     btn.onclick = function() {
         modal.style.display = "block";
 
     }
-    // When the user clicks anywhere outside of the modal, close it
+    //** When the user clicks anywhere outside of the modal, close it*/
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "block";
@@ -20,7 +20,7 @@ var modal = document.getElementById("myModal");
 let time = 0;
 let interval;
 
-//**  Create our html elements Cards with classes and ids*/
+//**  Create our html elements Cards with classes and ids */
 
 function createCard(object) {
     let li = document.createElement('li');
@@ -35,8 +35,8 @@ function createCard(object) {
     divFront.classList.add('flip-card-front');
 
     let imgFront = document.createElement('img');
-    imgFront.setAttribute('src', 'assets/images/front-side.png')
-    imgFront.setAttribute('data-name', object.name)
+    imgFront.setAttribute('src', 'assets/images/front-side.png');
+    imgFront.setAttribute('data-name', object.name);
     divFront.appendChild(imgFront);
 
 
@@ -44,7 +44,7 @@ function createCard(object) {
     divBack.classList.add('flip-card-back');
 
     let imgBack = document.createElement('img');
-    imgBack.setAttribute('src', object.img)
+    imgBack.setAttribute('src', object.img);
 
     divBack.appendChild(imgBack);
 
@@ -55,7 +55,7 @@ function createCard(object) {
     document.getElementById('cards-container').appendChild(li);
 }
 
-// Create an array of cards with image sources and names 
+//** Create an array of cards with image sources and names */
 
 let cardArray = [{
         name: 'cat',
@@ -98,50 +98,48 @@ let cardArray = [{
         img: 'assets/images/dugee.jpeg'
     },
 ]
-// Create runGame function so we can create the game board cards
+//** Create runGame function so we can create the game board cards */
 
 function runGame(length) {
     let number = length / 2
     let newArray = cardArray.slice(0, number)
-    // Sort the array in that way that it will always be random for the player
+    //** Sort the array in that way that it will always be random for the player */
     let doubledArray = newArray.concat(newArray).sort(() => Math.random() - 0.5);
-    let cardsCollection = document.getElementsByClassName('flip-card')
-    let cardsArray = [...cardsCollection]
+    let cardsCollection = document.getElementsByClassName('flip-card');
+    let cardsArray = [...cardsCollection];
     cardsArray.forEach((card) => {
-        card.remove()
+        card.remove();
 
     })
-    //Make the array of cards doubled in order to have a pair for each
+    //** Make the array of cards doubled in order to have a pair for each */
     for (i = 0; i < doubledArray.length; i++) {
-        createCard(doubledArray[i])
+        createCard(doubledArray[i]);
     }
-    // Create the click event to make the cards flip on click
+    //** Create the click event to make the cards flip on click */
 
-    cardsCollection = document.getElementsByClassName('flip-card')
-    cardsArray = [...cardsCollection]
+    cardsCollection = document.getElementsByClassName('flip-card');
+    cardsArray = [...cardsCollection];
     cardsArray.forEach((card) => {
         card.addEventListener('click', (event) => {
-            let cardElement = event.target.parentNode.parentNode
+            let cardElement = event.target.parentNode.parentNode;
 
             if (cardElement.style.transform !== 'rotateY(180deg)' && cardElement.classList.contains('flip-card-inner')) {
-                //Add audio file on click
+                //** Add audio file on click */
                 var audio = new Audio("Card-flip-sound-effect.mp3");
                 audio.play();
-                //Rotate the cards when clicked
-                cardElement.style.transform = 'rotateY(180deg)'
-                let selectedOption = event.target.getAttribute('data-name')
-                moves++
+                //** Rotate the cards when clicked */
+                cardElement.style.transform = 'rotateY(180deg)';
+                let selectedOption = event.target.getAttribute('data-name');
+                moves++;
                 document.getElementById('score').innerHTML = Math.floor(moves / 2);
                 setTimeout(() => {
-                    //If the cards are matched and there is none left the game will be finished
-                    matchOption(selectedOption)
+                    //** If the cards are matched and there is none left the game will be finished */
+                    matchOption(selectedOption);
                     if (cardsArray.length === cardsMatched * 2) {
-                        stopGame()
+                        stopGame();
                     }
 
-                }, 500)
-
-
+                }, 500);
 
             }
         })
@@ -149,27 +147,27 @@ function runGame(length) {
 
 
     });
-    startTime()
+    startTime();
 }
 
-//Create time variable for keeping the time for the player
+//** Create time variable for keeping the time for the player */
 function startTime() {
     clearTimeout(interval)
     time = 0;
     interval = setInterval(() => {
         time++;
-        document.getElementById('time').innerHTML = time + 's'
-    }, 500)
+        document.getElementById('time').innerHTML = time + 's';
+    }, 500);
 
 }
 
-//Declare variables which we will use to match the cards and count the moves
+//** Declare variables which we will use to match the cards and count the moves */
 let option1 = '';
 let option2 = '';
 let moves = 0
 let cardsMatched = 0
 
-//Create matchOption function so we can match our cards in pairs
+//** Create matchOption function so we can match our cards in pairs */
 
 function matchOption(selectedOption) {
     if (option1 === '') {
@@ -183,8 +181,8 @@ function matchOption(selectedOption) {
         if (option1 === option2) {
             option1 = '';
             option2 = '';
-            cardsMatched++
-            document.getElementById('matched-cards').innerHTML = cardsMatched
+            cardsMatched++;
+            document.getElementById('matched-cards').innerHTML = cardsMatched;
         } else {
             flipBack(option1);
             flipBack(option2);
@@ -196,40 +194,40 @@ function matchOption(selectedOption) {
 }
 
 
-//Create flipBack function so that our cards will flip back if they do not match
+//** Create flipBack function so that our cards will flip back if they do not match */
 
 function flipBack(option) {
     document.querySelectorAll(`img[data-name=${option}]`).forEach((card) => {
-        card.parentNode.parentNode.style.transform = 'rotateY(0deg)'
-    })
+        card.parentNode.parentNode.style.transform = 'rotateY(0deg)';
+    });
 
 }
 
-//Create functions to control levels of difficulty
-//Easy Level
+//** Create functions to control levels of difficulty */
+//** Easy Level */
 function easylevel() {
-    runGame(8)
+    runGame(8);
     modal.style.display = "none";
-    congrats.style.display = "none"
+    congrats.style.display = "none";
 }
-//Medium Level
+//** Medium Level */
 function mediumlevel() {
-    runGame(12)
+    runGame(12);
     modal.style.display = "none";
-    congrats.style.display = "none"
+    congrats.style.display = "none";
 }
-//Hard Level
+//** Hard Level */
 function hardlevel() {
-    runGame(16)
+    runGame(16);
     modal.style.display = "none";
-    congrats.style.display = "none"
+    congrats.style.display = "none";
 }
-//Game finished refresh everything and start again
+//** Game finished refresh everything and start again */
 
 function stopGame() {
-    document.getElementById('congrats').style.display = 'block'
+    document.getElementById('congrats').style.display = 'block';
 
-    clearTimeout(interval)
+    clearTimeout(interval);
 
     document.getElementById('result1').innerHTML = 'You have made :' + ' ' + moves / 2 + ' ' + 'moves';
     document.getElementById('result2').innerHTML = 'In' + ' ' + time + ' ' + 'seconds ';
